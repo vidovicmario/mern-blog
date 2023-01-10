52df04745f98eb6801981f3b22f241aee14fc49c
vidovicmario <vidovicmario97@gmail.com>
2023-01-10 12:39:53 +0100

    adding style to login and register pages

--- src/pages/RegisterPage.js
+++ src/pages/RegisterPage.js
@@ -1,11 +1,10 @@
 export default function RegisterPage() {
   return (
-    <div>
-      <form className="register">
-        <input type="text" placeholder="username" />
-        <input type="password" placeholder="password" />
-        <button>Register</button>
-      </form>
-    </div>
+    <form className="register">
+      <h1>Register</h1>
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button>Register</button>
+    </form>
   );
 }
